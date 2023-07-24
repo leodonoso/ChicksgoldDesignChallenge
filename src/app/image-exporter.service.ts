f4239c6911e54e2e1225bfc8ed428a0eb7539ce4
leodonoso <leonardoadonosog@gmail.com>
2023-07-24 19:17:39 -0400

    Finished Responsive Design

--- src/app/image-exporter.service.ts
+++ src/app/image-exporter.service.ts
@@ -5,14 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class ImageExporterService {
   sponsorLogos: any = [
-    {name: 'visa', url: '../../assets/logos/Visa_Inc._logo.png'},
-    {name: 'mastercard', url: '../../assets/logos/Mastercard-logo.png'},
-    {name: 'american-express', url: '../../assets/logos/american-express.png'},
-    {name: 'skrill', url: '../../assets/logos/Skrill_logo.png'},
-    {name: 'bitcoin-green', url: '../../assets/logos/Bitcoin-green.png'},
-    {name: 'ethereum', url: '../../assets/logos/Ethereum-Logo.png'},
-    {name: 'bitcoin', url: '../../assets/logos/Bitcoin.png'},
-    {name: 'litecoin', url: '../../assets/logos/litecoin.png'},
+    {name: 'visa', url: '../../assets/logos/Visa_Inc._logo.png', phone: true, tablet: true},
+    {name: 'mastercard', url: '../../assets/logos/Mastercard-logo.png', phone: true, tablet: true},
+    {name: 'american-express', url: '../../assets/logos/american-express.png', phone: false, tablet: true},
+    {name: 'skrill', url: '../../assets/logos/Skrill_logo.png', phone: false, tablet: true},
+    {name: 'bitcoin-green', url: '../../assets/logos/Bitcoin-green.png', phone: false, tablet: false},
+    {name: 'ethereum', url: '../../assets/logos/Ethereum-Logo.png', phone: false, tablet: false},
+    {name: 'bitcoin', url: '../../assets/logos/Bitcoin.png', phone: false, tablet: false},
+    {name: 'litecoin', url: '../../assets/logos/litecoin.png', phone: false, tablet: false},
   ]
 
   socialLogos: any = [
